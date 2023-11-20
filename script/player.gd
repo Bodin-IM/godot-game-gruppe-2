3f06ccb7b4b905fad4dc170c33451cb8aca01600
TeoRam23 <TeoRam23@vgs.nfk.no>
2023-11-20 13:49:07 +0100

    forbedret animasjon og begynte på attack

--- script/player.gd
+++ script/player.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var attack_area = $Area2D
+@onready var attack_shape = $Area2D/CollisionShape2D
+
+var is_attacking = false
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -10,12 +14,19 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	var direction = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down"))
+	var input_axis_x = Input.get_axis("left", "right")
+	var input_axis_y = Input.get_axis("up", "down")
+	if Input.is_action_just_pressed("attack"):
+		is_attacking = true
+	var direction = Vector2(input_axis_x, input_axis_y)
 	apply_movement(delta, direction)
+	
+	
+	handle_attack()
 
 	move_and_slide()
 	
-	play_animation(direction)
+	play_animation(direction, is_attacking)
 
 
 func apply_movement(delta, direction):
@@ -30,6 +41,29 @@ func apply_movement(delta, direction):
 		velocity.y = move_toward(velocity.x, 0, SPEED)
 
 
-func play_animation(direction):
-	if direction:
-		pass
+func handle_attack():
+	if is_attacking:
+		attack_area.position.x = 10.667
+		attack_shape.disabled = false
+		
+	else:
+		attack_area.position.x = 0
+		attack_shape.disabled = true
+
+
+
+
+func play_animation(direction, is_attacking):
+	if is_attacking:
+		animated_sprite_2d.play("attack")
+	if not is_attacking:
+		if direction:
+			animated_sprite_2d.play("walk")
+			animated_sprite_2d.flip_h = (direction.x < 0)
+		else:
+			animated_sprite_2d.play("idle")
+	
+
+
+func _on_animated_sprite_2d_animation_finished():
+	is_attacking = false
