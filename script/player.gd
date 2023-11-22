784a3ac92f729a19109f68dc32187002f6e6c7ef
TeoRam23 <TeoRam23@vgs.nfk.no>
2023-11-22 11:23:59 +0100

    attack
    gjorde ferdig attack funksjon
    la til opp/ned attack sprites

--- script/player.gd
+++ script/player.gd
@@ -7,7 +7,8 @@ const SPEED = 300.0
 @onready var attack_shape = $Area2D/CollisionShape2D
 
 var is_attacking = false
-
+var moving_vertical = false
+var vertical_direction = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -22,11 +23,11 @@ func _physics_process(delta):
 	apply_movement(delta, direction)
 	
 	
-	handle_attack()
+	handle_attack(direction)
 
 	move_and_slide()
 	
-	play_animation(direction, is_attacking)
+	update_animation(direction, is_attacking)
 
 
 func apply_movement(delta, direction):
@@ -41,25 +42,50 @@ func apply_movement(delta, direction):
 		velocity.y = move_toward(velocity.x, 0, SPEED)
 
 
-func handle_attack():
+func handle_attack(direction):
 	if is_attacking:
-		attack_area.position.x = 10.667
+		if animated_sprite_2d.flip_h:
+			attack_area.position.x = -10.667
+			attack_area.position.y = 0
+		elif not moving_vertical:
+			attack_area.position.x = 10.667
+			attack_area.position.y = 0
+		elif vertical_direction:
+			attack_area.position.y = -6
+			attack_area.position.x = 0
+		else:
+			attack_area.position.y = 6
+			attack_area.position.x = 0
+			
+			
 		attack_shape.disabled = false
 		
 	else:
 		attack_area.position.x = 0
+		attack_area.position.y = 0
 		attack_shape.disabled = true
 
 
 
 
-func play_animation(direction, is_attacking):
+func update_animation(direction, is_attacking):
 	if is_attacking:
-		animated_sprite_2d.play("attack")
-	if not is_attacking:
+		if not moving_vertical:
+			animated_sprite_2d.play("attack")
+		elif vertical_direction:
+			animated_sprite_2d.play("attack_up")
+		else:
+			animated_sprite_2d.play("attack_down")
+	else:
 		if direction:
 			animated_sprite_2d.play("walk")
 			animated_sprite_2d.flip_h = (direction.x < 0)
+			moving_vertical = (direction.y != 0)
+			vertical_direction = (direction.y < 0)
+		elif Input.is_action_pressed("block"):
+			animated_sprite_2d.play("block?")
+			
+			
 		else:
 			animated_sprite_2d.play("idle")
 	
