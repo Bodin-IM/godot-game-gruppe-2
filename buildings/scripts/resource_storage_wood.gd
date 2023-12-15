634f0431d29b1232a9a32c40288259a073572ac4
T-mann6 <113894304+T-mann6@users.noreply.github.com>
2023-12-15 11:33:23 +0100

    hard stuff

--- buildings/scripts/resource_storage_wood.gd
+++ buildings/scripts/resource_storage_wood.gd
@@ -1,8 +1,1 @@
 extends Node2D
-
-var max_hp : int = 100
-var current_hp : int = max_hp
-
-func update_hp_bar():
-	var hp_percentage : float = current_hp / max_hp
-	$ColorRect.rect_size.x = $Sprite.texture.get_width() * hp_percentage
