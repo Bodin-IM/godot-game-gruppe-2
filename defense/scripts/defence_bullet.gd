bb8998ba7abb2044aa751f02a7feb36fb3d9c138
TeoRam23 <TeoRam23@vgs.nfk.no>
2024-01-08 16:00:24 +0100

    defense tower komplett og bullet sprite

--- defense/scripts/defence_bullet.gd
+++ defense/scripts/defence_bullet.gd
@@ -1,30 +1,41 @@
 extends Area2D
 
-var speed = 300
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+@export var speed = 300
 var attack = 3
 
 var orm_pos
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	look_at(orm_pos)
+	animated_sprite_2d.play("chomp")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	move(delta)
+	
 
 func move(delta):
 #	global_position.x = move_toward(global_position.x, orm_pos.x, speed * delta)
 #	global_position.y = move_toward(global_position.y, orm_pos.y, speed * delta)
-	global_position = global_position.move_toward(orm_pos, delta*speed)
+	position += transform.x * speed * delta
+
+
+
+	# gjør at den spinner, veldig kult
+#	look_at(orm_pos)
+#	position += transform.y * speed * delta
+	
 	
 	
 	
 func look_towards(target):
-	print(target)
-	look_at(target)
+	print("target")
 	orm_pos = target
+	look_at(orm_pos)
 
 
 func _on_body_entered(body):
@@ -32,3 +43,8 @@ func _on_body_entered(body):
 		print("traff orm fra defense")
 		body.on_hit(attack)
 		queue_free()
+
+
+func _on_timer_timeout():
+	# gjør at den ikke eksisterer for alltid
+	queue_free()
