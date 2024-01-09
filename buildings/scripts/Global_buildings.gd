70a20ac7ea0b4ff953a7b964a503506c79f3a0f8
TeoRam23 <teoram23@vgs.nfk.no>
2024-01-09 12:30:47 +0100

    fikset progressbar, bygg kan bygges på nytt

--- buildings/scripts/Global_buildings.gd
+++ buildings/scripts/Global_buildings.gd
@@ -1,12 +1,52 @@
-extends Node2D
+extends Area2D
 
-var hp = 100
+@onready var progressbar = $Sprite2D/ProgressBar
+
+var label
+var wood_price
+var stone_price
+var max_hp = 100
+var what_build
+var hp
+
+var plot = preload("res://scenes/build_plot.tscn")
+
+func _ready():
+	hp = max_hp
+	progressbar.max_value = max_hp
+
+func _physics_process(delta):
+	progressbar.value = hp
+	
+	if (hp > max_hp * 0.6): 
+		progressbar.modulate = "#00ff00"
+	
+	if (hp < max_hp * 0.6):
+		progressbar.modulate = "#ffff00"
+		
+	if (hp < max_hp * 0.3):
+		progressbar.modulate = "#de0000"
+	
+
 
 func on_hit():
 	hp -= 1
 	if hp <= 0:
-		destroyed()
+		call_deferred("destroyed")
+	
 
 func destroyed():
+	if label:
+		var new_plot = plot.instantiate()
+		new_plot.global_position = global_position
+		
+		new_plot.label = label
+		new_plot.wood_price = wood_price
+		new_plot.stone_price = stone_price
+		new_plot.hp = max_hp
+		new_plot.what_build = what_build
+		get_parent().get_parent().add_child(new_plot)
 	get_parent().queue_free()
 	print("Crumbled")
+
+
