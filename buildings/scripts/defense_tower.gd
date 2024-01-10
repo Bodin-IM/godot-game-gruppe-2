9791b2fe2f70ffa41fc5b62d4925afcecfa467bf
TeoRam23 <teoram23@vgs.nfk.no>
2024-01-10 12:48:22 +0100

    fikset defense tower
    defense toweret er tilbake til å skyte krabbeklør

--- buildings/scripts/defense_tower.gd
+++ buildings/scripts/defense_tower.gd
@@ -3,7 +3,7 @@ extends Node2D
 var target = null
 var attackRange = 200
 var shooting_delay = 1.0  # Set the delay between shots
-var damage_per_shot = 5
+var damage_per_shot = 2
 
 var time_since_last_shot = 0.0
 
@@ -11,29 +11,31 @@ var can_shoot = true
 var bullet = preload("res://defense/scenes/defence_bullet.tscn")
 var shoot_sfx = preload("res://audio/defenseShoot.wav")
 
-func _process(delta):
-	if target != null:
-#		rotate_towards_target(target.position)
 
 func _ready():
 	$Timer.start()
 	
-func _process(_delta):
-	if target:
-		rotate_towards_target(target.position)
+#func _process(_delta):
+func _process(delta):
+	if target != null:
+#		rotate_towards_target(target.position)
 		# Implement shooting logic
-		if can_shoot:
-			print("shot")
+#		if can_shoot:
+#			print("shot")
+#			shoot()
+		time_since_last_shot += delta
+		if time_since_last_shot >= shooting_delay:
 			shoot()
-			
-	reset_target()
-	# Check for enemies in range and set as target
+			time_since_last_shot = 0.0
+			# Check for enemies in range and set as target
 	var enemies = get_tree().get_nodes_in_group("enemy_group")
 	for enemy in enemies:
 		var distance = global_position.distance_to(enemy.global_position)
 		if distance < attackRange and not target:
 			set_target(enemy)
 			break
+			
+#	reset_target()
 
 func rotate_towards_target(target_position):
 	var angle = position.angle_to(target_position)
